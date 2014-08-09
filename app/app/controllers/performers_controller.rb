e4dde2bb5f95b15e256788fc153410bf8ebb4d53
Matias Insaurralde <matias@insaurral.de>
2014-08-09 14:47:21 -0400

    lookup action

--- app/app/controllers/performers_controller.rb
+++ app/app/controllers/performers_controller.rb
@@ -1,4 +1,8 @@
 class PerformersController < ApplicationController
   def index
   end
+  def lookup
+    listener_position = params[:position]
+    # you need to POST to /lookup with param 'position', this param will be the value of navigator.geolocation.getCurrentPosition
+  end
 end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'performers#index'
+  post 'performers/lookup' => 'performers#lookup'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
