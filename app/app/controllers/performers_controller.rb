38ad3a12f64501743441cc3086ef3f8aac7ca5ec
Matias Insaurralde <matias@insaurral.de>
2014-08-09 15:55:11 -0400

    performer json reply is fine

--- app/app/controllers/performers_controller.rb
+++ app/app/controllers/performers_controller.rb
@@ -3,6 +3,7 @@ class PerformersController < ApplicationController
   end
   def lookup
     listener_position = params[:position]
+    render :json => Performer.all.map { |p| { 'id' => p.id, 'name' => p.name } }
     # you need to POST to /lookup with param 'position', this param will be the value of navigator.geolocation.getCurrentPosition
   end
 end
