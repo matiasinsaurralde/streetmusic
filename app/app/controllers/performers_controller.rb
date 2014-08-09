8f3aa38526d4d4ffded1cfebdde0dd25c7b766e1
Victor Jiao <VjiaoBlack@gmail.com>
2014-08-09 16:23:58 -0400

    edited performers contorller

--- app/app/controllers/performers_controller.rb
+++ app/app/controllers/performers_controller.rb
@@ -1,8 +1,32 @@
 class PerformersController < ApplicationController
-  def index
-  end
-  def lookup
-    listener_position = params[:position]
-    # you need to POST to /lookup with param 'position', this param will be the value of navigator.geolocation.getCurrentPosition
-  end
+    def index
+    end
+
+    def create
+    end
+
+    def new
+    end
+
+    def view
+        # when you see an artist page
+        # allows to donate
+    end
+
+    def edit
+        # an artist can edit his profile
+    end
+
+    def delete
+        # delete yourself from database?
+    end
+
+    def lookup
+        listener_position = params[:position]
+        # you need to POST to /lookup with param 'position', this param will be the value of navigator.geolocation.getCurrentPosition
+    end
+
+
+
+
 end
