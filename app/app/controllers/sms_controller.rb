bf533c243924139c6d9cf8fff811384ee8b9c690
Matias Insaurralde <matias@insaurral.de>
2014-08-10 08:54:38 -0400

    fixing twilio

--- app/app/controllers/sms_controller.rb
+++ app/app/controllers/sms_controller.rb
@@ -1,14 +1,14 @@
 class SmsController < ApplicationController
   skip_before_filter :force_ssl # You may need this if your app uses https normally
+  TWILIO_SID = 'AC2a3e7b30d28bae4ca313834c1c07bd63'
+  TWILIO_AUTH_TOKEN = 'de74b27c8306aab8f8544e652d2fb515'
 
   def create
-    # Do something with params['From'] -- contains the phone number the SMS came from
-    # Do something with params['Body'] -- contains the text sent in the SMS
-
-    # <Reponse/> is the minimum to indicate a "no response" from Twilio
-    render xml: "<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-    <Message>Hey Monkey. Thanks for the message!</Message>
-</Response>"
+    client = Twilio::REST::Client.new(TWILIO_SID, TWILIO_AUTH_TOKEN)
+    client.account.messages.create(
+      :from => '+14432192344',
+      :to => '+14438516506',
+      :body => 'Hey there!'
+    )
   end
 end
