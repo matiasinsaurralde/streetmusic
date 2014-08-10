aa843589b66aa9f9cf3d3d36da83e84f8adfc774
Matias Insaurralde <matias@insaurral.de>
2014-08-09 23:26:47 -0400

    modifying deploy settings

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,58 +1,76 @@
-# config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'streetmusic'
+set :repo_url, 'git@github.com:matiasinsaurralde/streetmusic.git'
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+set :deploy_to, '/var/www/streetmusic'
+set :scm, :git
+set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
+set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
 
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :rails_env, 'production'
 
-# Default value for :scm is :git
-# set :scm, :git
+set :user, 'streetmusic'
+set :group, 'streetmusic'
+set :runner, 'streetmusic'
 
 # Default value for :format is :pretty
 # set :format, :pretty
-
 # Default value for :log_level is :debug
 # set :log_level, :debug
-
 # Default value for :pty is false
 # set :pty, true
-
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
-
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
-  desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
+    on roles(:app) do
+      execute "if [ -f #{fetch(:unicorn_pid)} ]; then kill -USR2 `cat #{fetch(:unicorn_pid)}`; else cd #{fetch(:deploy_to)}/current && bundle exec unicorn -c #{fetch(:unicorn_conf)} -E #{fetch(:rails_env)} -D; fi"
+    end
+  end
+  task :start do
+    on roles(:app) do
+      execute "cd #{fetch(:deploy_to)}/current && bundle exec unicorn -c #{fetch(:unicorn_conf)} -E #{fetch(:rails_env)} -D"
+    end
+  end
+  task :stop do
+    on roles(:app) do
+      execute "if [ -f #{fetch(:unicorn_pid)} ]; then kill -QUIT `cat #{fetch(:unicorn_pid)}`; fi"
+    end
+  end
+
+  task :reset_db do
+    on roles(:app) do
+      execute "cd #{fetch(:deploy_to)}/current && bundle exec rake db:drop RAILS_ENV=#{fetch(:rails_env)}"
+      execute "cd #{fetch(:deploy_to)}/current && bundle exec rake db:create RAILS_ENV=#{fetch(:rails_env)}"
+      execute "cd #{fetch(:deploy_to)}/current && bundle exec rake db:migrate RAILS_ENV=#{fetch(:rails_env)}"
+    end
+  end
+
+  #desc 'Restart application'
+  #task :restart do
+  #  on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
+  #  end
+  #end
 
-  after :publishing, :restart
+  #after :publishing, :restart
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
+  #after :restart, :clear_cache do
+  #  on roles(:web), in: :groups, limit: 3, wait: 10 do
+  #    # Here we can do anything such as:
+  #    # within release_path do
+  #    #   execute :rake, 'cache:clear'
+  #    # end
+  #  end
+  #end
 end
--- app/config/deploy/development.rb
+++ app/config/deploy/development.rb
@@ -4,10 +4,7 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-
-set :rails_env, 'staging'
-
-role :app, %w{cassette@cassette}
+# role :app, %w{cassette@cassette}
 #role :web, %w{deploy@example.com}
 #role :db,  %w{deploy@example.com}
 
